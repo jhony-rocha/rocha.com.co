8ec3d6754788a12390f7181c6742fe91b04a4586
jhony-rocha <jhony.alexander.rocha@gmail.com>
2025-02-11 12:11:51 -0500

    Add coverage to test

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,17 @@ const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true, 
+  coverageDirectory: "coverage", 
+  coverageReporters: ["text", "lcov"], 
+  coverageThreshold: {  
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 }
 
 module.exports = createJestConfig(customJestConfig)
